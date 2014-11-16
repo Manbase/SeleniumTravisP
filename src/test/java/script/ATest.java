54d8181f98dc014dda660ae526b3566eef8cc95b
ughosh <ghosh_ujwl@live.com>
2014-11-15 20:51:36 -0500

    First Commit for SeleniumTravisP Project

---
+++ src/test/java/script/ATest.java
@@ -0,0 +1,38 @@
+package script;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ATest {
+
+	
+	WebDriver driver = null;
+	
+	@BeforeMethod
+	public void b(){
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(1, TimeUnit.MINUTES);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void main() {
+		
+		driver.navigate().to("http://www.bn.com");
+
+
+	}
+	@AfterMethod
+	public void a(){
+		driver.close();
+	}
+
+
+
+}
